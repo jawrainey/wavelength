241637cbce16241f7d39281945393efdf595f1b5
Aare Puussaar <a.puussaar2@ncl.ac.uk>
2016-04-17 14:45:10 +0100

    redirect in error wtf

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def selection():
     if request.method == 'POST':
         selected_imgs = request.get_json()['imgs']
         # TODO: Receive selected images & populate 'selected_imgs'
-        print 'Now we want to redirect... blah'
+        return redirect(url_for('input'))
     return render_template('selection.html', imgs=files)
 
 
